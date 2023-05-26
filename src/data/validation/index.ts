81c2bbaa5c1142e572cdbfe687b13bf314c9305a
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2023-05-27 00:28:14 +0300

    moved validation schemas to separate module

--- src/data/validation/index.ts
+++ src/data/validation/index.ts
@@ -1,31 +1,5 @@
 import { ZodError, z } from "zod";
-
-const journeySchema = z.object({
-  departure: z.coerce.date(),
-  return: z.coerce.date(),
-  departureStationId: z.coerce.number(),
-  departureStationName: z.coerce.string(),
-  returnStationId: z.coerce.number(),
-  returnStationName: z.coerce.string(),
-  coveredDistanceInMeters: z.coerce.number().gte(10),
-  durationInSeconds: z.coerce.number().gte(10),
-});
-
-const stationSchema = z.object({
-  fid: z.coerce.number(),
-  stationId: z.coerce.number(),
-  name: z.coerce.string(),
-  nameSwe: z.coerce.string(),
-  nameEn: z.coerce.string(),
-  address: z.coerce.string(),
-  addressSwe: z.coerce.string(),
-  city: z.coerce.string().nullable(),
-  citySwe: z.coerce.string().nullable(),
-  operator: z.coerce.string().nullable(),
-  capacity: z.coerce.number(),
-  latitude: z.coerce.number().gte(-90).lte(90),
-  longitude: z.coerce.number().gte(-180).lte(180),
-});
+import { journeySchema, stationSchema } from "./validationSchema";
 
 type Journey = z.infer<typeof journeySchema>;
 type Station = z.infer<typeof stationSchema>;
