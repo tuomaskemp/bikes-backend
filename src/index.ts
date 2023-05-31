3fc3908bd42e7ce1b94410610fc3ec2ba0a0c12c
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2023-05-31 17:44:57 +0300

    added cors and updated journeys route to return 100 items

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { stationsRouter } from "./routes/stations";
 import { journeysRouter } from "./routes/journeys";
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use("/api/stations", stationsRouter);
 app.use("/api/journeys", journeysRouter);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
