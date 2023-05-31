6303acb904a0ee7438fddcef010d083ce1cc782a
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2023-05-31 18:36:27 +0300

    added single station route

--- src/routes/stations.ts
+++ src/routes/stations.ts
@@ -10,4 +10,32 @@ router.get("/", async (_req: Request, res: Response) => {
   res.status(200).json(result);
 });
 
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params["id"];
+    const singleStation = await prisma.station.findUniqueOrThrow({
+      where: { stationId: Number(id) },
+    });
+    if (singleStation) {
+      const totalJourneysDeparting = await prisma.journey.count({
+        where: {
+          departureStationId: singleStation.id,
+        },
+      });
+      const totalJourneysReturning = await prisma.journey.count({
+        where: {
+          returnStationId: singleStation.id,
+        },
+      });
+      res.status(200).json({
+        ...singleStation,
+        totalJourneysDeparting,
+        totalJourneysReturning,
+      });
+    }
+  } catch (error: unknown) {
+    res.status(404);
+  }
+});
+
 export { router as stationsRouter };
